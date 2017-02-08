ee4e2d31b7511c0ae94684691b7c20cc2f7ebfa5
ORO-PC006\nguye <nguyenthithanhloan@oro.vn.com>
2017-02-08 17:20:05 +0700

    upload

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require('gulp')
 var pug = require('gulp-pug');
 // css (sass)
 var sass = require('gulp-sass');
-
-
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
 /* pug to HTML*/
 gulp.task('pug', function() {
 	gulp.src('./src/*.pug')
@@ -12,11 +12,33 @@ gulp.task('pug', function() {
 		pretty: true
 	}))
 	.pipe(gulp.dest('./dist'))
-})
+	.pipe(browserSync.stream({ once: true }))
+});
 
+// =============================================
+// browser-sync
+//
+gulp.task('browser-sync', () => {
+  browserSync.init(CONFIG.browserSync)
+});
 
 gulp.task('watch', function() {
 	gulp.watch('./src/*.pug', ['pug'])
 });
+
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "D:/education/nm.phanthiet.mn/dist/"
+        }
+    });
+
+    gulp.watch("*.html").on("change", reload);
+});
+
 // auto review
-gulp.task('default', ['pug', 'watch']);
+gulp.task('default', ['pug', 'watch', 'serve']);
